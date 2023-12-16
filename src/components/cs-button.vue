41ce255f2403c51d7d5b147004098d011c32bd09
Squizy <squizyace@gmail.com>
2023-12-17 00:59:40 +0700

    btn/header commit

---
+++ src/components/cs-button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="cs-button">
+    <csIcon v-if="icon" :iconName="icon" />
+    <span><slot/></span>
+  </button>
+</template>
+<script>
+import csIcon from './icons.vue';
+export default {
+  components: {
+    csIcon
+  },
+  props:{
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    type: {
+      type: String,
+      default: ''
+    },
+    icon: {
+      type: String,
+      default: undefined
+    }
+  }
+}
+</script>
